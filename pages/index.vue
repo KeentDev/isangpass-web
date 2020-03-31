4772ac03673cb8ab738766e592533bf8b1760a97
Keanu Kent B. Gargar <keanukent.dev@gmail.com>
2020-03-31 16:56:50 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="row center">
+    Initial Setup.
+  </div>
+</template>
+
+<script>
+import {fireDb} from '~/plugins/firebase.js'
+
+export default {}
+</script>
+
+<style lang="scss">
+</style>

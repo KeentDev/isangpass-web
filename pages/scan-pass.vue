fbb154b2ab6642639b6b41c2ca405fa1385c6d3a
Keanu Kent B. Gargar <keanukent.dev@gmail.com>
2020-04-13 20:21:45 +0800

    Error messages setup

--- pages/scan-pass.vue
+++ pages/scan-pass.vue
@@ -15,8 +15,10 @@
       <div class="qr_code-cntr col center" v-if="isCameraOn || isProcessingQR || showScanDetails">
         <qrcode-stream class="qr_code-stream" :camera="camera" @decode="onDecode" @init="onInit">
         </qrcode-stream>
-        <div class="loading-label">
-          Loading...
+        <div class="overlay-label">
+          <div class="loading-label" v-if="errorMsg == ''">
+            Loading...
+          </div>
         </div>
         <div v-if="showOverlay" class="qr_code-overlay-cntr col center">
           <div class="qr_code-overlay-text qr_code-overlay--verifying row center">
@@ -34,24 +36,29 @@
                   :status="ctaStatus"
                   @btnClick="approvePass"
                   :btnLabel="ctaLabel"
-                  :successMsg="successMsg">
+                  :successMsg="successMsg"
+                  :errorMsg="ctaErrorMsg">
                   <template v-slot:label-icon></template>
                 </OPCtaButton>
               </div>
             </div>
-            <template v-if="!showScanDetails">
+            <template v-if="!showScanDetails && !errorMsg">
               {{overlayStatusText}}
             </template>
+            <div class="error-msg-cntr col center" v-if="errorMsg != ''">
+              <div class="error-msg">
+                {{errorMsg}}
+              </div>
+              <button
+                class="error-msg-cancel btn btn-secondary"
+                @click="cancelError"
+              >Scan</button>
+            </div>
           </div>
         </div>
         <div :class="{'is_validating': showOverlay}" class="qr_code-footer row pull-apart">
           <div class="qr_code-status qr_code-status--scanning">
-            <template v-if="!showScanDetails">
-              Scanning
-            </template>
-            <template v-if="showScanDetails">
-              Authenticated
-            </template>
+            {{scanFooterLabel}}
           </div>
           <div class="qr_code-location">Brgy. {{scanBarangay}}</div>
         </div>
@@ -96,7 +103,10 @@
       ctaStatus: '',
       submitIsDisable: false,
       ctaLabel: '',
-      successMsg: 'Success'
+      successMsg: 'Success',
+      errorMsg: '',
+      scanFooterLabel: 'Scanning',
+      ctaErrorMsg: null
     }
   },
 
@@ -214,21 +224,22 @@
             return Promise.resolve({duration: 1500, showDetails: false});
           }else {
             this.ctaStatus = 'error';
-            return Promise.reject();
+            return Promise.reject(response.data.error.error_msg);
           }
         })
         .catch(e => {
           this.ctaStatus = 'error';
-
+          this.ctaErrorMsg = e;
           console.error(e);
-          return Promise.resolve({duration: 3500, showDetails: true});
+          return Promise.resolve({duration: 3000, showDetails: true});
         })
         .then(async (options) => {
           await setTimeout(() => {
             this.ctaStatus = '';
             this.successMsg = 'Success';
+            this.ctaErrorMsg = '';
             this.showScanDetails = options.showDetails;
-          }, options.duration || 3000);
+          }, options.duration || 2500);
         });
     },
 
@@ -240,8 +251,10 @@
       let barangay;
       this.result = content
       this.isProcessingQR = true;
+
       this.turnCameraOff()
 
+      this.scanFooterLabel = 'Authenticating';
       this.showScanDetails = false;
 
       try {
@@ -266,12 +279,14 @@
             .catch(e => {
               return Promise.reject(e)
             })
-            .then(response => {
+            .then(async response => {
               if(response.data.success) {
                 const data = response.data.data;
                 const firstName = data.holder.first_name;
                 const lastName = data.holder.last_name;
                 const passType = data.pass_type;
+
+                this.showScanDetails = true;
 
                 if(passType == 'check_out') {
                   this.ctaLabel = 'Check-out';
@@ -281,39 +296,50 @@
                   this.successMsg = 'Check-in successful';
                 }
 
-                this.showScanDetails = true;
-
                 this.scanFirstName = firstName;
                 this.scanLastName = lastName;
                 this.scanPassType = passType;
                 this.scanOnePassKey = onePasskey;
+                this.scanFooterLabel = 'Authenticated';
+
+                await this.timeout(5000)
+
+                this.showOverlayStatus = false;
+                this.isProcessingQR = false;
+
+                return Promise.resolve();
+              }else {
+                return Promise.reject(response.data.error.error_msg);
               }
             })
             .catch(e => {
-              return Promise.reject(e)
+              throw e
             })
         } else {
-          throw 'Invalid number of pass parameters.'
+          throw 'Invalid number of pass format.'
         }
 
         this.isValid = true;
       } catch (error) {
         console.error('Something went wrong. Please try again.', error);
-
+        this.scanFooterLabel = 'Scan failed.';
+        this.errorMsg = error;
         this.isValid = false;
       }
-
-      await this.timeout(2000)
-      this.showOverlayStatus = false;
-
-
-      this.isProcessingQR = false;
       this.overlayStatusText = 'Loading...';
+    },
+
+    cancelError () {
+      this.errorMsg = '';
+      this.showOverlayStatus = false;
+      this.isProcessingQR = false;
+      this.toggleCamera();
     },
 
     turnCameraOn () {
       this.isCameraOn = true,
       this.camera = 'auto'
+      this.scanFooterLabel = 'Scanning';
     },
 
     turnCameraOff () {
@@ -421,7 +447,6 @@
   }
   .qr_code-placeholder-label {
     font-size: 20px;
-    // font-weight: 500;
     color: $color__slate-gray;
   }
   .btn_camera-on {
@@ -430,13 +455,15 @@
     color: $color__green-primary;
   }
   .loading-label {
+    color: $color__slate-1;
+  }
+  .overlay-label {
     z-index: 1;
     position: absolute;
     top: 50%;
     left: 50%;
     font-weight: 500;
     font-size: 20px;
-    color: $color__slate-1;
     transform: translate(-50%, -50%);
   }
   .scan_details-overlay-cntr {
@@ -491,5 +518,19 @@
   .btn-actions-cntr {
     width: 100%;
     margin-top: $space__base-5
+  }
+
+  .error-msg-cntr {
+    width: 100%;
+    padding: $space__base-4 $space__base-5;
+    background-color: rgba(0,0,0,.55);
+    border-radius: $border_radius-1;
+    color: $color__red-secondary;
+    font-size: 24px;
+    font-weight: 600;
+  }
+
+  .error-msg-cancel {
+    margin-top: $space__base-3;
   }
 </style>
